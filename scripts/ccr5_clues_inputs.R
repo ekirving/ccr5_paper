a21f198c248a01d277be3a22a9689c1d1ae8a815
Evan Irving-Pease <evan.irvingpease@gmail.com>
2023-04-11 15:09:03 +0100

    applied tidyverse style guide

--- scripts/ccr5_clues_inputs.R
+++ scripts/ccr5_clues_inputs.R
@@ -6,7 +6,7 @@
 # License:   MIT
 
 quiet <- function(x) {
-  suppressMessages(suppressWarnings(x))
+    suppressMessages(suppressWarnings(x))
 }
 quiet(library(readr))
 quiet(library(dplyr))
@@ -15,21 +15,21 @@ quiet(library(stringr))
 
 # load the CCR5 deletion calls
 ccr5_calls <- read_tsv("data/ancient_samples_09_11_updated.tsv", col_types = cols()) %>%
-  replace_na(list(Notes="")) %>%
-  select(Sample, Predicted_by_model, Artifacts_filter, Minus_haplo_filter, pRR_Data, pRD_Data, pDD_Data, Notes)
+    replace_na(list(Notes = "")) %>%
+    select(Sample, Predicted_by_model, Artifacts_filter, Minus_haplo_filter, pRR_Data, pRD_Data, pDD_Data, Notes)
 
 # load the sample metadata
 meta.neo <- read_tsv("data/neo.impute.1000g.sampleInfo.orig.tsv", guess_max = 100000, col_types = cols()) %>%
-  mutate(age = ifelse(groupAge == "Modern", 0, ageAverage))
+    mutate(age = ifelse(groupAge == "Modern", 0, ageAverage))
 
 # join the sample metadata
 ccr5_neo <- ccr5_calls %>%
-  inner_join(
-    select(meta.neo, sampleId, age),
-    by = c("Sample" = "sampleId")
-  ) %>%
-  mutate(gens = age / 28) %>%
-  arrange(gens)
+    inner_join(
+        select(meta.neo, sampleId, age),
+        by = c("Sample" = "sampleId")
+    ) %>%
+    mutate(gens = age / 28) %>%
+    arrange(gens)
 
 # --------------------------------------------------------------
 # convert the 5 different callsets into CLUES input format
@@ -37,43 +37,43 @@ ccr5_neo <- ccr5_calls %>%
 
 # the pRR_Data / pRD_Data / pDD_Data
 ccr5_neo %>%
-  mutate(call = sprintf("%.6f %.6f %.6f", log10(pRR_Data), log10(pRD_Data), log10(pDD_Data))) %>%
-  mutate(clues = sprintf("%.6f %s", gens, call)) %>%
-  select(clues) %>%
-  write_delim("clues/p_Data/ccr5-p_Data.ancient", col_names = F, quote_escape = F, delim = "")
+    mutate(call = sprintf("%.6f %.6f %.6f", log10(pRR_Data), log10(pRD_Data), log10(pDD_Data))) %>%
+    mutate(clues = sprintf("%.6f %s", gens, call)) %>%
+    select(clues) %>%
+    write_delim("clues/p_Data/ccr5-p_Data.ancient", col_names = F, quote_escape = F, delim = "")
 
 # Predicted_by_model
 ccr5_neo %>%
-  mutate(call = case_when(
-    Predicted_by_model == "RR" ~ "0.000000 -inf -inf",
-    Predicted_by_model == "RD" ~ "-inf 0.000000 -inf",
-    Predicted_by_model == "DD" ~ "-inf -inf 0.000000"
-  )) %>%
-  mutate(clues = sprintf("%.6f %s", gens, call)) %>%
-  select(clues) %>%
-  write_delim("clues/Predicted_by_model/ccr5-Predicted_by_model.ancient", col_names = F, quote_escape = F, delim = "")
+    mutate(call = case_when(
+        Predicted_by_model == "RR" ~ "0.000000 -inf -inf",
+        Predicted_by_model == "RD" ~ "-inf 0.000000 -inf",
+        Predicted_by_model == "DD" ~ "-inf -inf 0.000000"
+    )) %>%
+    mutate(clues = sprintf("%.6f %s", gens, call)) %>%
+    select(clues) %>%
+    write_delim("clues/Predicted_by_model/ccr5-Predicted_by_model.ancient", col_names = F, quote_escape = F, delim = "")
 
 # Artifacts_filter
 ccr5_neo %>%
-  mutate(call = case_when(
-    Artifacts_filter == "RR" ~ "0.000000 -inf -inf",
-    Artifacts_filter == "RD" ~ "-inf 0.000000 -inf",
-    Artifacts_filter == "DD" ~ "-inf -inf 0.000000"
-  )) %>%
-  mutate(clues = sprintf("%.6f %s", gens, call)) %>%
-  select(clues) %>%
-  write_delim("clues/Artifacts_filter/ccr5-Artifacts_filter.ancient", col_names = F, quote_escape = F, delim = "")
+    mutate(call = case_when(
+        Artifacts_filter == "RR" ~ "0.000000 -inf -inf",
+        Artifacts_filter == "RD" ~ "-inf 0.000000 -inf",
+        Artifacts_filter == "DD" ~ "-inf -inf 0.000000"
+    )) %>%
+    mutate(clues = sprintf("%.6f %s", gens, call)) %>%
+    select(clues) %>%
+    write_delim("clues/Artifacts_filter/ccr5-Artifacts_filter.ancient", col_names = F, quote_escape = F, delim = "")
 
 # Minus_haplo_filter
 ccr5_neo %>%
-  mutate(call = case_when(
-    Minus_haplo_filter == "RR" ~ "0.000000 -inf -inf",
-    Minus_haplo_filter == "RD" ~ "-inf 0.000000 -inf",
-    Minus_haplo_filter == "DD" ~ "-inf -inf 0.000000"
-  )) %>%
-  mutate(clues = sprintf("%.6f %s", gens, call)) %>%
-  select(clues) %>%
-  write_delim("clues/Minus_haplo_filter/ccr5-Minus_haplo_filter.ancient", col_names = F, quote_escape = F, delim = "")
+    mutate(call = case_when(
+        Minus_haplo_filter == "RR" ~ "0.000000 -inf -inf",
+        Minus_haplo_filter == "RD" ~ "-inf 0.000000 -inf",
+        Minus_haplo_filter == "DD" ~ "-inf -inf 0.000000"
+    )) %>%
+    mutate(clues = sprintf("%.6f %s", gens, call)) %>%
+    select(clues) %>%
+    write_delim("clues/Minus_haplo_filter/ccr5-Minus_haplo_filter.ancient", col_names = F, quote_escape = F, delim = "")
 
 # minus_haplotype
 ccr5_neo %>%
--- scripts/ccr5_mutation_age.R
+++ scripts/ccr5_mutation_age.R
@@ -6,7 +6,7 @@
 # License:   MIT
 
 quiet <- function(x) {
-  suppressMessages(suppressWarnings(x))
+    suppressMessages(suppressWarnings(x))
 }
 quiet(library(readr))
 quiet(library(dplyr))
@@ -24,36 +24,30 @@ MIN_FREQ <- 1.112985e-04 # second lowest possible value!
 
 clues_trajectory <- function(model_path) {
 
-  # load the model data
-  epochs <- npyLoad(paste0(model_path, ".epochs.npy"))
-  freqs <- npyLoad(paste0(model_path, ".freqs.npy"))
-  logpost <- npyLoad(paste0(model_path, ".post.npy"), dotranspose = F)
+    # load the model data
+    epochs <- npyLoad(paste0(model_path, ".epochs.npy"))
+    freqs <- npyLoad(paste0(model_path, ".freqs.npy"))
+    logpost <- npyLoad(paste0(model_path, ".post.npy"), dotranspose = F)
 
-  # add column names
-  colnames(logpost) <- paste0("V", seq(ncol(logpost)))
+    # add column names
+    colnames(logpost) <- paste0("V", seq(ncol(logpost)))
 
-  model <- as_tibble(logpost) %>%
+    model <- as_tibble(logpost) %>%
+        # convert posterior densities from log-likelihoods
+        exp() %>%
+        # add the frequency labels
+        add_column(freq = freqs, .before = 2) %>%
+        # add the title heights (and a little padding to make sure there are no gaps)
+        # tile heights are not equal because there is higher sampling density near 0 and 1
+        add_column(height = diff(c(0, freqs)) + 1e-4, .before = 3) %>%
+        # pivot the columns into long format
+        pivot_longer(-c(freq, height), names_to = "epoch", values_to = "density") %>%
+        # convert the column names into epochs (and switch the direction of time)
+        mutate(epoch = -epochs[as.numeric(gsub("V", "", epoch))]) %>%
+        # sort by epoch age
+        arrange(epoch)
 
-    # convert posterior densities from log-likelihoods
-    exp() %>%
-
-    # add the frequency labels
-    add_column(freq = freqs, .before = 2) %>%
-
-    # add the title heights (and a little padding to make sure there are no gaps)
-    # tile heights are not equal because there is higher sampling density near 0 and 1
-    add_column(height = diff(c(0, freqs)) + 1e-4, .before = 3) %>%
-
-    # pivot the columns into long format
-    pivot_longer(-c(freq, height), names_to = "epoch", values_to = "density") %>%
-
-    # convert the column names into epochs (and switch the direction of time)
-    mutate(epoch = -epochs[as.numeric(gsub("V", "", epoch))]) %>%
-
-    # sort by epoch age
-    arrange(epoch)
-
-  model
+    model
 }
 
 models <- c("p_Data", "Predicted_by_model", "Artifacts_filter", "Minus_haplo_filter", "minus_haplotype")
@@ -62,25 +56,25 @@ modes <- c("mod", "no_mod")
 
 # load the trajectories
 traj <- bind_rows(
-  lapply(models, function(model) {
-    bind_rows(
-      lapply(epochs, function(epoch_type) {
-        lapply(modes, function(mode) {
-          clues_trajectory(paste0("clues/", model, "/ccr5-", model, "-", epoch_type, "-", mode)) %>%
-            mutate(model = paste0(model, "-", epoch_type, "-", mode))
-        })
-      })
-    )
-  })
+    lapply(models, function(model) {
+        bind_rows(
+            lapply(epochs, function(epoch_type) {
+                lapply(modes, function(mode) {
+                    clues_trajectory(paste0("clues/", model, "/ccr5-", model, "-", epoch_type, "-", mode)) %>%
+                        mutate(model = paste0(model, "-", epoch_type, "-", mode))
+                })
+            })
+        )
+    })
 )
 
 age <- traj %>%
-  filter(freq <= MIN_FREQ) %>%
-  group_by(model, epoch) %>%
-  summarise(density = sum(density), freq = max(freq)) %>%
-  filter(density >= 0.5) %>%
-  slice(which.max(epoch)) %>%
-  mutate(age_bp = -epoch * 28) %>%
-  select(model, freq, epoch, age_bp, density)
+    filter(freq <= MIN_FREQ) %>%
+    group_by(model, epoch) %>%
+    summarise(density = sum(density), freq = max(freq)) %>%
+    filter(density >= 0.5) %>%
+    slice(which.max(epoch)) %>%
+    mutate(age_bp = -epoch * 28) %>%
+    select(model, freq, epoch, age_bp, density)
 
 write_tsv(age, "clues/ccr5_mutation_ages.tsv")
