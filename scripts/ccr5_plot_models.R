122f1737e813869e0b5107c40c8a98088267f460
Evan Irving-Pease <evan.irvingpease@gmail.com>
2023-04-11 15:08:29 +0100

    updated plot labels

--- scripts/ccr5_plot_models.R
+++ scripts/ccr5_plot_models.R
@@ -25,14 +25,18 @@ gen_time <- 28
 # frequency of the deletion in FIN, GBR and TSI
 modern_freq <- 0.1237
 
-models <- c("Predicted_by_model", "Artifacts_filter", "Minus_haplo_filter")
+models <- c(
+    # "Predicted_by_model", # not included in the main text
+    "Artifacts_filter", # Permissive filter
+    "Minus_haplo_filter" # Strict filter
+)
 epochs <- c("one-epoch")
 modes <- c("mod", "no_mod")
 
 # load the estimate allele ages
 ages <- read_tsv("clues/ccr5_mutation_ages.tsv", col_types = cols()) %>%
-    separate(col=model, into = c("model", "epochs", "_", "mode"), sep="-") %>%
-    filter(model %in% models & epochs=="one" & mode %in% modes)
+    separate(col = model, into = c("model", "epochs", "_", "mode"), sep = "-") %>%
+    filter(model %in% models & epochs == "one" & mode %in% modes)
 
 
 clues_trajectory <- function(model_path, smooth = 10) {
@@ -46,23 +50,17 @@ clues_trajectory <- function(model_path, smooth = 10) {
     colnames(logpost) <- paste0("V", seq(ncol(logpost)))
 
     model <- as_tibble(logpost) %>%
-
         # convert posterior densities from log-likelihoods
         exp() %>%
-
         # add the frequency labels
         add_column(freq = freqs, .before = 2) %>%
-
         # add the title heights (and a little padding to make sure there are no gaps)
         # tile heights are not equal because there is higher sampling density near 0 and 1
         add_column(height = diff(c(0, freqs)) + 1e-4, .before = 3) %>%
-
         # pivot the columns into long format
         pivot_longer(-c(freq, height), names_to = "epoch", values_to = "density") %>%
-
         # convert the column names into epochs (and switch the direction of time)
         mutate(epoch = -epochs[as.numeric(gsub("V", "", epoch))]) %>%
-
         # sort by epoch age
         arrange(epoch)
 
@@ -91,7 +89,17 @@ traj <- bind_rows(
                     data <- fromJSON(paste0(prefix, ".json"))
                     pval <- formatC(pchisq(2 * data$logLR, df = 1, lower.tail = FALSE), format = "e", digits = 1)
                     clues_trajectory(prefix) %>%
-                        mutate(model = model, label = paste0(model, " (p=", pval, ")"), epoch_type = epoch_type, mode = mode)
+                        mutate(
+                            model = model,
+                            name = case_when(
+                                model == "Artifacts_filter" ~ "Permissive filter",
+                                model == "Minus_haplo_filter" ~ "Strict filter",
+                                TRUE ~ model
+                            ),
+                            label = paste0(name, " (p=", pval, ")"),
+                            epoch_type = epoch_type,
+                            mode = mode
+                        )
                 })
             })
         )
@@ -109,7 +117,6 @@ traj$mode <- factor(traj$mode, levels = c("mod", "no_mod"), labels = c("With mod
 ages$mode <- factor(ages$mode, levels = c("mod", "no_mod"), labels = c("With modern ascertainment", "Without modern ascertainment"))
 
 plt <- traj %>%
-
     # plot the trajectories
     ggplot(aes(x = epoch, y = freq, color = model)) +
 
@@ -123,7 +130,7 @@ plt <- traj %>%
     geom_line() +
 
     # show the allele ages
-    geom_point(aes(y=0), ages) +
+    geom_point(aes(y = 0), ages) +
 
     # set the model colours
     # scale_color_manual(values = snp_colors) +
@@ -133,8 +140,9 @@ plt <- traj %>%
 
     # set the axis breaks
     scale_y_continuous(limits = c(0, .25), breaks = seq(0, 1, .05), position = "left") +
-    scale_x_continuous(limits = c(xmin, xmax), breaks = xbreaks, labels = xlabels, expand = expansion(add = c(0, 520))) +
+    scale_x_continuous(limits = c(xmin, xmax), breaks = xbreaks, labels = xlabels, expand = expansion(add = c(0, 450))) +
 
+    # set the labels
     labs(title = "CCR5-delta 32") +
     ylab("DAF") +
     xlab("kyr BP") +
@@ -149,4 +157,4 @@ plt <- traj %>%
         panel.background = element_blank()
     )
 
-ggsave("figure/ccr5_delta32_trajectory.png", plt, width=9, height=4)
+ggsave("figure/ccr5_delta32_trajectory.png", plt, width = 9, height = 4)
